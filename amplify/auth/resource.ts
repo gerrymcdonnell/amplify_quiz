85006c8ca0f5c4931b58e0a58c6070a491e53bfa
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2026-01-31 18:04:04 +0000

    modified resourse.ts per tutorial

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,17 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: "Welcome to quiz app! Verify your email!",
+      verificationEmailBody: (code) =>
+        `Here is your verification code: ${code()}`,
+      verificationEmailStyle: "CODE",
+    },
+  },
+  userAttributes: {
+    preferredUsername: {
+      mutable: true,
+      required: true,
+    },
   },
 });
